03b6fe9d60f733b44cfd0696fc3714bfbd3c3e55
Guest User <student@epicodus.com>
2015-11-03 16:33:37 -0800

    Add testing.

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 describe Game do
   it { should have_many :guesses }
+
+  it 'should split the answer attribute into an array of letters' do
+    test_game = Game.create(answer: 'abcd')
+    expect(test_game.split_answer_to_letters()).to(eql(['A', 'B', 'C', 'D', ' '])) 
+  end
 end
--- spec/models/guess_spec.rb
+++ spec/models/guess_spec.rb
@@ -3,4 +3,11 @@ require 'rails_helper'
 describe Guess do
   it { should belong_to :game }
   it { should validate_presence_of :letter }
+
+  it 'check the guess a user inputs to see if it is part of the answer' do
+    test_game = Game.create(answer: 'abcd')
+    test_guess = test_game.guesses.new(letter: 'A')
+    test_guess.check_guess
+    expect(test_guess.in_answer?).to(eql(true))
+  end
 end
