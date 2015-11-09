e877290e989aeb3760df0f7b0f7ce86b5cbfa3c2
Alyssa Horrocks <am.rocks@outlook.com>
2015-11-09 17:14:39 -0500

    Allow access to player on game#show.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,16 @@
 class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
+    @player = Player.where(id: (params[:player]))[0]
   end
 
   def create
     @game = Game.new(style: params[:commit])
+    player = Player.where(name: (params[:player]))
+    @player = player[0]
+
     if @game.save
-      redirect_to game_path(@game)
+      redirect_to game_path(@game, player: @player)
     else
       render :new
     end
